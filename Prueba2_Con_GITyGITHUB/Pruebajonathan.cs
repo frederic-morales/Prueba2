8b18196279eab1eb9766ac0935c21a0cd250d71f
jonathan7w7rrr <jonathan7w7rrr@gmail.com>
2024-10-05 10:28:02 -0600

    Mi primer proyecto jonathan

---
+++ Prueba2_Con_GITyGITHUB/Pruebajonathan.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prueba2_Con_GITyGITHUB
+{
+    internal class Pruebajonathan
+    {
+        public Pruebajonathan() { }
+        public int IdStatus;
+        public
+    }
+}
